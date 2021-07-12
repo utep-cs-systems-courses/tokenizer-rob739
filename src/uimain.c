632cdca4fad73dded49e85c8678a5fe70a2bac95
Roberto Carrasco <rcarrasco8@miners.utep.edu>
2021-07-12 00:29:55 -0600

    Simple Ui added in main

---
+++ src/uimain.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+/*#include "tokenizer.h"
+#include "history.h"*/
+
+
+int main()
+{
+  char input[100];
+
+  while(input[0] != '@'){
+    printf(">");
+    //scanf("%s",input);
+    fgets(input,sizeof(input),stdin);
+    printf("%s",input);
+    
+  }
+   
+  return 0;
+
+
+}
